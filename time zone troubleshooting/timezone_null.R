d43e873bc856bd8c07be7b27e0c7ca5a51f97de4
Jon Bryant <jon@jonbryant.co>
2025-03-06 12:01:17 -0500

    Adding timezone testing file

---
+++ time zone troubleshooting/timezone_null.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+library(readxl)
+library(DBI)
+library(fs)
+library(pool)
+
+# Import excel data
+#cs1_24q1 <- read_excel("Data/1267-2-1_CS1_24Q1_15min_QAQC_20240509_SPM.xlsx")
+
+conn <- dbPool(
+  drv = RPostgres::Postgres(),
+  host = "192.168.131.120",
+  port = 5432,
+  dbname = "CentralDB",
+  user= Sys.getenv("central_uid"),
+  password = Sys.getenv("central_pwd"),
+  timezone = NULL)
+
+
+# Get data from centralDB
+# conn <- dbConnect(odbc::odbc(), dsn = "CentralDB", uid = Sys.getenv("central_uid"), 
+#                   pwd = Sys.getenv("central_pwd"))
+fromCentral <- dbGetQuery(conn, "SELECT * FROM pwdrg.tblmodelrain")
+poolClose(conn)
+
+# Organize data from centralDB
+names(fromCentral)
+filtered_central <- fromCentral |> 
+  filter(DateTime >= "2022-01-01") |>
+  select(GaugeNo, DateTime, Rainfall)
+names(filtered_central) <- c("gage", "time", "rainfall")
+
+conn <- dbPool(
+  drv = RPostgres::Postgres(),
+  host = "PWDMARSDBS1",
+  port = 5434,
+  dbname = "mars_data",
+  user= Sys.getenv("shiny_uid"),
+  password = Sys.getenv("shiny_pwd"),
+  timezone = NULL)
+# Get data from MARS
+# conn <- dbConnect(odbc::odbc(), dsn = "mars", uid = Sys.getenv("shiny_uid"), pwd = Sys.getenv("shiny_pwd"))
+fromMars <- dbGetQuery(conn, "SELECT * FROM data.tbl_gage_rain")
+poolClose(conn)
+
+# Organize Data from MARS
+filtered_mars <- fromMars |> filter(dtime_edt >= "2022-01-01") |> select(gage_uid, dtime_edt, rainfall_in)
+names(filtered_mars) <- c("gage", "time", "rainfall")
+
+# Check differences
+differences <- symdiff(filtered_mars, filtered_central)
+
+
+# Differences between MARS and CentalDB
+print(differences[,2])
+
+conn <- dbPool(
+  drv = RPostgres::Postgres(),
+  host = "PWDMARSDBS1",
+  port = 5434,
+  dbname = "Jon_sandbox",
+  user= Sys.getenv("shiny_uid"),
+  password = Sys.getenv("shiny_pwd"),
+  timezone = NULL)
+
+dbWriteTable(conn, "tbl_test_date", filtered_mars |> select(time, rainfall), append = TRUE)
